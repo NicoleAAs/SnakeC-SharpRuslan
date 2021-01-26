55aa0efd411668c13d18d099f003837629313451
Nixgui <robert.ecward@gmail.com>
2021-01-26 21:18:57 +0200

    Первый урок

--- Program.cs
+++ Program.cs
@@ -6,30 +6,24 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class Progra
+    class Program
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1,y1,sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point pl = new Point();
+            pl.x = 1;
+            pl.y = 3;
+            pl.sym = '*';
+            pl.Draw();
             
+            Point pl2=new Point();
+            pl2.x = 4;
+            pl2.y = 5;
+            pl2.sym = '#';
+            pl2.Draw();
             
             Console.ReadLine();
         }
-
-        static void Draw(int x, int y, char sym )
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+        
     }
 }
