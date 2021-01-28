7bf924b8091f12a86154dab8bbf01f48048572dd
Nixgui <robert.ecward@gmail.com>
2021-01-28 15:36:54 +0200

    Added Snake and Snake move

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,13 +11,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point pl = new Point(1,3,'*');
-            pl.Draw();
-            
-            Point pl2=new Point(4,5,'#');
-            pl2.Draw();
-            
-            Console.ReadLine();
+           //Console.SetBufferSize(80,25);
+
+           HLine upLine = new HLine(0,78,0,'-');
+           HLine downLine= new HLine(0,78,24,'-');
+           VLine leftLine = new VLine(0,24,0,'|');
+           VLine rightLine = new VLine(0,24,78,'|');
+           upLine.Draw();
+           downLine.Draw();
+           leftLine.Draw();
+           rightLine.Draw();
+
+           Point p = new Point(4, 5, '*');
+           Snake snake = new Snake(p, 4, Direction.RIGHT);
+           snake.Draw();
+           snake.Move();
         }
         
     }
---
+++ Snake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Snake
+{
+    public class Snake : Figure
+    {
+        Direction direction;
+        public Snake(Point tail, int lenght, Direction _direction)
+        {
+            direction = _direction;
+            plist = new List<Point>();
+            for (int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.move(i, direction);
+                plist.Add(p);
+            }
+        }
+
+        internal void Move()
+        {
+            Point tail =plist.First();
+            plist.Remove(tail);
+            Point head = GetNextPoint();
+            plist.Add(head);
+            
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = plist.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.move (1, direction);
+            return nextPoint;
+        }
+    }
+}
